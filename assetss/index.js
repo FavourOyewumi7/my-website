a39105de9df5c05360abdcd0e6ba19f80752a391
FavourOyewumi7 <oyewumifavour65@gmail.com>
2020-12-09 17:52:25 +0100

    added new edits, almost ready now

--- assetss/index.js
+++ assetss/index.js
@@ -1,9 +1,10 @@
 let names = ['Front End Web Developer', 'Machine Learning Enthusiast', 'Photographer']
-console.log(names.length)
-let tag = document.getElementsByClassName('words')[0]
-
-
-
+console.log(names.length);
+let tag = document.getElementsByClassName('words')[0];
+let nav = document.getElementsByClassName('width')[0];
+let lists = document.getElementsByClassName('nav-lists')[0];
+let bar = document.getElementsByClassName('bars')[0]
+let lis = document.getElementsByClassName('nav-list')[0];
 
 
 myWords(names,tag)
@@ -16,3 +17,12 @@ function myWords(names, target) {
 
 
 new WOW().init();
+
+nav.addEventListener("click", showNav);
+
+function showNav(){
+ 
+    bar.style.display = 'none';
+    lists.style.display = 'block';
+    lis.style.display = 'none'
+}
