9273126e72ddfb599c3481b19fb157d68e148253
Favour Oyewumi <oyewumifavour65@gmail.com>
2023-02-15 11:42:33 +0100

    added vercel analytics

--- new-portfolio/src/main.js
+++ new-portfolio/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { inject } from '@vercel/analytics';
 
+inject();
 
-createApp(App).mount('#app')
+createApp(App).mount('#app').use(inject)
